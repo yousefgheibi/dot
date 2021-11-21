4a16ff658dc0bcfa1f5d0b4ef3f1157d06627416
Yousef <gheibiyousef2000@gmail.com>
2021-11-21 09:18:24 +0330

    Solve the challenge

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 document.querySelector(".btn").addEventListener("click", () => {
-  //Your Code
+  let count = 0;
+  let str = document.querySelector("input").value;
+  str = str.replace(/(^\s*)|(\s*$)/gi, "");
+  str = str.replace(/[ ]{2,}/gi, " ");
+  str = str.replace(/\n /, "\n");
+
+  for (let i = 0; i < str.length; i++) {
+    for (let [key, value] of Object.entries(obj)) {
+      if (key == str[i]) {
+        count = count + f(value);
+      }
+    }
+  }
+  document.querySelector(".result").innerHTML = count;
 });
